5d0f17626a167fcbbf28fae0174261c93ab69f04
ZEROwolf <1059901966@qq.com>
2018-01-29 10:40:52 +0800

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './Appa';
+
+AppRegistry.registerComponent('DrawerStackTabNavigator', () => App);
---
+++ srca/Shop1.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    TouchableOpacity,
+    View,
+    StyleSheet
+}from 'react-native';
+
+export default class Type1 extends Component {
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.button}
+                                  activeOpacity={0.5}
+                                 >
+                    <Text>
+                        首页
+                    </Text>
+                </TouchableOpacity>
+
+            </View>
+        );
+    }
+
+};
+const styles = StyleSheet.create({
+
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#099'
+        },
+        button: {
+            width:125,
+            height:80,
+            borderRadius:10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#00f'
+        },
+
+    }
+);
